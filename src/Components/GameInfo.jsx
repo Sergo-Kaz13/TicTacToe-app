833ff759b065d40f5c45be5dd80c75eb95d1665a
Sergo_Kaz <wwe619dx@ukr.net>
2025-05-15 19:48:54 +0200

    chore: add homepage field in package.json for GitHub Pages deployment

--- src/Components/GameInfo.jsx
+++ src/Components/GameInfo.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import userIcon from "../../public/assets/user_icon-icons.com_57997.png";
 
 const GameInfo = ({ user }) => {
   const stylesUser = {
-    base: "text-lg font-semibold",
-    player: "text-2xl text-yellow-500",
+    base: "text-lg font-semibold inline-flex items-center gap-5",
+    player: "text-3xl text-blue-700",
+    userIcon: `h-5`,
+    userBlock: "inline-flex items-center gap-1",
   };
 
   return (
     <h2 className={`${stylesUser.base}`}>
-      Гравець <span className={`${stylesUser.player}`}>{user}</span>
+      <span className={`${stylesUser.userBlock}`}>
+        <img className={`${stylesUser.userIcon}`} src={userIcon} alt="user" />
+        Гравець
+      </span>
+      <span className={`${stylesUser.player}`}>{user}</span>
     </h2>
   );
 };
